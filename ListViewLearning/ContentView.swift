bc11099d26b8a3f915020891eac9918396849458
Devesh Joshi <joshi.devesh71@gmail.com>
2024-04-23 18:22:59 +0530

    Initial Commit

---
+++ ListViewLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ListViewLearning
+//
+//  Created by Infoicon on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ListViewLearning/ListViewLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewLearningApp.swift
+//  ListViewLearning
+//
+//  Created by Infoicon on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ListViewLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
